a84eedcaacf976d24d743634990091abe1ac6575
Jared Selcoe <jared.orion.selcoe@gmail.com>
2020-10-20 18:11:08 -0500

    Refactor to support multiple fix scripts

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 require('dotenv').config();
 
 const fix = require('./lib/fix');
+const fixTwo = require('./lib/fixTwo');
 
 process.on('uncaughtException', err => {
   console.error({ err }, 'Uncaught Exception Trapped');
 });
 
-fix();
+if (process.env.TO_RUN === 'fix') fix();
+else if (process.env.TO_RUN === 'fixTwo') fixTwo();
